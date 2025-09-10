90c1e7dd29dfd252e92e874be3488c666a2bcc87
leti74 <letiimar4@gmail.com>
2025-09-10 16:34:00 +0200

    Updates

---
+++ assets/WhyPage-D0ET_sl_.js
@@ -0,0 +1,33 @@
+import{j as e,H as l}from"./index-CSGO-ov5.js";import{N as h,F as d}from"./Footer-BWY1BCCm.js";const t=({whyIMG:n,whyParagraph:i,whyTesto:s,whyTitle1:r,whyTitle2:o,whyStyle:a})=>e.jsxs(e.Fragment,{children:[e.jsxs("div",{className:"div1-why",children:[e.jsx("div",{className:"background-img-why",children:e.jsx("img",{src:n,alt:"img",className:"img-div1"})}),e.jsxs("div",{className:`div1-text ${a}`,children:[e.jsx("h2",{children:r}),e.jsx("p",{children:s})]})]}),e.jsxs("div",{className:`div2-why ${a}`,children:[e.jsx("h3",{children:o}),e.jsx("p",{children:i})]})]}),u=()=>e.jsxs(e.Fragment,{children:[e.jsxs(l,{children:[e.jsx("title",{children:"Perché Scegliere una Dieta Vegetale | EasyVeg"}),e.jsx("meta",{name:"description",content:"Scopri i motivi etici, ambientali e salutistici per adottare una dieta vegetale."})]}),e.jsx(h,{}),e.jsx(t,{whyIMG:"/vegetarian-recipes/farms.jpg",whyTitle1:"Why animals deserve fairness.",whyTesto:`We often distinguish between companions and farm animals, but\r
+        this division is artificial. A pig can be as affectionate\r
+        as a dog, and a cow can bond deeply with its\r
+        baby. Only some animals receive love, while others are\r
+        exploited. Changing perspective means recognizing that all\r
+        animals feel, suffer, and desire to live.\r
+        With a plant-based diet, we can help save hundreds\r
+        of lives every year. It's a powerful gesture that starts with a simple\r
+        choice.`,whyTitle2:`The true cost of\r
+        meat.`,whyParagraph:`In Italy alone, hundreds of millions of animals are raised and killed every year for food. Behind the\r
+        packaging on the shelves, there are stories of chickens grown\r
+        in less than two months, rabbits killed at three months, and cows that\r
+        don't even live a quarter of the time that\r
+        nature would allow them. The places where they are raised are\r
+        often crowded, devoid of stimulation, and full of invisible\r
+        suffering. In those facilities, time stands still: they\r
+        survive, waiting for the day when it will all end. To\r
+        prevent dangerous behaviors caused by the stress of\r
+        overcrowding, the animals are mutilated: beaks, tails, horns.\r
+        Pigs are castrated without anesthesia. These procedures\r
+        are performed on sentient beings, who feel pain. In narrow\r
+        cages, bare pens, or sheds, the animals cannot\r
+        express their natural behaviors. They live deprived of everything\r
+        that would make their lives worth living. Fish\r
+        are often the great forgotten ones. Despite feeling pain,\r
+        they are raised in densely overcrowded conditions, immersed\r
+        in polluted or stagnant water. Often, there are no\r
+        regulations regarding their slaughter: they are left to die\r
+        slowly, out of the water, so as not to drown our\r
+        consciences. Yet, they too fight for life.`,whyStyle:"animal-theme"}),e.jsx(t,{whyIMG:"/vegetarian-recipes/environment.jpg",whyTitle1:"Because it's our planet and our responsibility",whyTesto:"The Earth is our only home, and caring for it is a choice that goes far beyond our present: it means protecting every living being that inhabits it. And while we're taught as children to turn off the lights or not waste water, no one explains to us that the most powerful gesture we make every day: choosing what to eat.",whyTitle2:"The enormous impact on the planet.",whyParagraph:`Imagine millions of animals turning plant-based feed into meat, milk, or eggs every day. It seems simple, but it's one of the most inefficient energy conversions in existence: we use enormous quantities of grains and legumes to feed animals instead of directly feeding them to people. Globally, most of the soy grown ends up in animal feed, and only a tiny fraction reaches our tables. A plant-based diet, by contrast, uses fewer resources and contributes to a more equitable use of food on Earth. When we talk about climate change, we think of factories and cars. But there's another source, quieter but even more impactful: livestock farming. Methane and nitrous oxide emissions from animals are among the most dangerous for global warming.\r
+        We're told to turn off the faucet while brushing our teeth to save water—a helpful gesture, but insignificant compared to the amount of water needed to produce meat and animal products. Each hamburger requires thousands of liters of water, not only for the animal itself but also to grow its food and manage the waste it produces. Shifting our choices towards a plant-based diet means concretely reducing our water impact, every day.`,whyStyle:"planet-theme"}),e.jsx(t,{whyIMG:"/vegetarian-recipes/wellbeing.jpg",whyTitle1:"Because our health depends on the food we eat and the environment we live in.",whyTesto:"Following a plant-based diet is a recommended nutritional strategy at all stages of life. The latest scientific research confirms that a plant-based diet can offer numerous benefits for overall health, if well-balanced and planned. Numerous medical and nutritional professionals affirm that properly structured vegan and vegetarian diets are perfectly nutritionally adequate and can positively contribute to the prevention and management of many chronic diseases.",whyTitle2:"The power of a plant-based diet.",whyParagraph:`Cardiovascular diseases remain a leading cause of death globally. A healthy lifestyle, including a diet rich in plant-based foods, can play a crucial role in reducing risk factors such as high cholesterol and high blood pressure.\r
+        Furthermore, regular consumption of fruits, vegetables, whole grains, legumes, and nuts is a powerful weapon against type 2 diabetes and obesity, often aggravated by excessive consumption of meat and simple sugars. Intensive livestock farming has negative effects not only on animals, but also on humans and the environment. The systemic use of antibiotics in livestock farming contributes to antimicrobial resistance, while pollution from animal waste and deforestation compromises air and water quality.\r
+        Furthermore, most emerging infectious diseases originate from close contact between humans and animals, often facilitated by intensive livestock farming and the destruction of natural habitats. It is precisely in these contexts that viruses make the so-called species leap, generating new zoonoses.`,whyStyle:"health-theme"}),e.jsx(d,{})]});export{u as default};
